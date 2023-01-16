2cd0331995df489961521b45f22c0acec40d47b4
Stefan Siegler <stefan@siegler.ch>
2023-01-16 13:37:36 +0100

    V0.9.4

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
 # import
 from setuptools import setup
+from mt5_scraper import __version__
 
 # setup
 setup(
     name='mt5_scraper',
-    version='0.9.3',
+    version=__version__,
     url='https://github.com/XO30/MT5Scraper',
     author='Stefan Siegler',
     author_email='dev@siegler.one',
     py_modules=['mt5_scraper'],
     classifiers=[
         'Intended Audience :: Data Scientists',
-
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     install_requires=[
         'anytree',
         'pandas',
         'tqdm',
         'MetaTrader5',
-    ]
+        'numpy==1.24.0'  # required by MetaTrader5
+    ],
+    python_requires='>3.5, <4',
 )
